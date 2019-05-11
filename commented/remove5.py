ad1f8663c18429eeeeaa67a011424834f721e09f
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2019-05-11 16:51:59 +0530

    Create remove5.py

---
+++ commented/remove5.py
@@ -0,0 +1,14 @@
+newred = []
+red5 = (len(red)/100)*5
+for i in range(red5,len(red)-5):
+    newred.append(red[i])
+
+newblue= []
+blue5 = (len(blue)/100)*5
+for i in range(blue5,len(blue)-5):
+    newblue.append(blue[i])
+
+newgreen= []
+green5 = (len(green)/100)*5
+for i in range(red5,len(green)-5):
+    newgreen.append(green[i])
