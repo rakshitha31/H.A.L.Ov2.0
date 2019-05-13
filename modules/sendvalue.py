61a868d9943045b3bce268762df4ed1c0596517b
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2019-05-13 19:29:25 +0530

    Update sendvalue.py

--- modules/sendvalue.py
+++ modules/sendvalue.py
@@ -4,5 +4,6 @@ from firebase import firebase
 sendvalue(): # Function to send the average value of heartrate to firebase
   firebase=firebase.FirebaseApplication('https://halo2-1ce47.firebaseio.com/')
   result = firebase.put('/Patients/Lohith','heartRate',math.ceil(avg))
-  
+  avg = ( max0 + max1 )/2  #calculates the average heartbeat of the blue and hue signal
+
 sendvalue()
