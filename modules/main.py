219cee6b6e1c78a59e2ebf1088fa388ef0920b4c
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2019-05-13 19:28:27 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -7,36 +7,9 @@ from scipy.fftpack import fft
 import numpy as np
 from scipy.fftpack import fftfreq
 import matplotlib.pyplot as plt
-
 import getvideo
 import variables
-
-video_capture = cv2.VideoCapture("rashiiii.mp4")
-#video_capture.set(cv2.CV_CAP_PROP_FPS,60)
-# Run the infinite loop
-ret = True
-
-while ret:
-  try:
-  # Read each frame
-      ret, frame = video_capture.read()
-      #print(frame.shape)
-    #  if frame.empty():
-    #      break;
-      
-      frame = cv2.resize(frame,(int(frame.shape[1]/2),int(frame.shape[0]/2)))
-      frame = imutils.rotate_bound(frame,90)
-      # Convert frame to grey because cascading only works with greyscale image
-      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-      # Call the detect function with grey image and colored frame
-      canvas = detectface(gray, frame)
-      if cv2.waitKey(1) and 0xFF == ord('q'):
-        break
-  except AttributeError:
-      break      
-video_capture.release()
-cv2.destroyAllWindows()
-
+import loadvideo
 import getdifference
 import detrend
 import performfft
